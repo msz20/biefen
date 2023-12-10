ea1ad1273334c71c089c9a2729eb803a48b34ef9
mashuoze <msz20110214@outlook.com>
2023-12-10 18:17:41 +0800

    python

---
+++ python/masing.py
@@ -0,0 +1,13 @@
+massages = ['jack', 'alice', 'bob', 'helen']
+for massage in massages:
+    print(f"我不想错过你们的演出, {massages}\n")
+
+    print("谢谢各位,真是一场很棒的表演!")
+
+
+magicians = ['alice', 'david', 'carolina']
+  for magician in magicians:
+      print(f"{magician.title()}, that was a great trick!")
+      print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!"
---
+++ python/pizza.py
@@ -0,0 +1,6 @@
+massages = ('芝士披萨', '奶酪披萨', '容颜披萨')
+
+for massages in massages:
+    print(f"我喜欢{massages.title()}\n")
+
+    print("I really like pizza!")
