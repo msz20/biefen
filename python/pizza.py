7e5c5a8ea1bad27e6bbff8b53007e6bf8f9ee292
mashuoze <msz20110214@outlook.com>
2023-12-16 17:37:27 +0800

    python

--- python/pizza.py
+++ python/pizza.py
@@ -3,4 +3,4 @@ massages = ('芝士披萨', '奶酪披萨', '容颜披萨')
 for massages in massages:
     print(f"我喜欢{massages.title()}\n")
 
-    print("I really like pizza!")
+print("I really like pizza!")
---
+++ python/计算面积.py
@@ -0,0 +1,6 @@
+longth = float(input('请输入长度:'))
+width = float(input('请输入宽度:'))
+
+area= longth * width
+
+print(area)
