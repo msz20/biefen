4bda8b57d863149df89f35f38e2bae8ce67bac12
mashuoze <msz20110214@outlook.com>
2023-12-16 18:00:36 +0800

    python

--- python/购物问题.py
+++ python/购物问题.py
@@ -1,1 +1,7 @@
-price = float(input("请输入你要购买的物品的价格："))
+thing = input('请输入你要购买的物品：')
+price = int (input("请输入你要购买的物品的价格："))
+quan = int (input('请输入你要购买的物品的数量：'))
+area = price * quan
+
+
+print(f'您购买的{thing}的总价是{area}美元。')
