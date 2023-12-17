7ebb2398a7a9ed029db7e330b7c7c871314ac0fc
mashuoze <msz20110214@outlook.com>
2023-12-17 17:39:03 +0800

    python

--- python/想法.py
+++ python/想法.py
@@ -10,4 +10,14 @@ elif massage == '王克伟':
 else:
     print('你所输入的的内容无效.')
 # 有一个bug,这个文件在执行完white后,在输入人名,就不会执行if语句,会直接印出内容.
-# 修好了.
+# 修好了,花了我20分钟时间.
+# 下面是第一次的代码写的跟坨屎似的.
+# massage = input('请输入人名:')
+# if massage == 'mashuoze':
+#   print('说真的,我不知道自己是怎么想的,要写这鬼东西.')
+# elif massage == '王丽格':
+#   print('妈,你好吗?我不好.')
+# elif massage == '王克伟':
+#   print('Hello! What fuck?')
+# while massage ='':
+#    print('请输入您的名字')
