20cd7cfb35ee894dca646584f759018c578ea3d4
mashuoze <msz20110214@outlook.com>
2023-12-31 20:58:55 +0800

    python

--- python/加减乘除计算器.py
+++ python/加减乘除计算器.py
@@ -1,7 +1,7 @@
-symbol = input('请输入计算符号')
-num1 = float(input('请输入第一个数字'))
-num2 = float(input('请输入第二个数字'))
-small = 1
+symbol = input('请输入计算符号:')
+num1 = float(input('请输入第一个数字:'))
+num2 = float(input('请输入第二个数字:'))
+small = float
 if symbol == '+':
     small = num1 + num2
 elif symbol == '-':
