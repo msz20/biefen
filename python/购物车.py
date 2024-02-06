75f17f38c183eea5311cfe24f60ebff8c2a77a38
mashuoze <msz20110214@outlook.com>
2024-02-06 08:11:46 +0800

    python

--- python/购物车.py
+++ python/购物车.py
@@ -1,8 +1,8 @@
 Menu = {
-    "披萨" : 100,
-    "可乐" : 60,
-    "爆米花" :70,
-    "薯条" : 90,
+    "披萨": 100,
+    "可乐": 60,
+    "爆米花": 70,
+    "薯条": 90,
 }
 cart = []
 Total = 0
@@ -13,10 +13,10 @@ for item, price in Menu.items():
 while True:
     food = input("请输入种类（按Q结束）")
     if food == 'Q':
-      break
+        break
     elif Menu.get(food) is None:
-       print('菜单中无此商品。')
+        print('菜单中无此商品。')
     else:
-       cart.append(food)
-       Total = Menu.get(food)
+        cart.append(food)
+        Total = Menu.get(food)
 print(f'总共为{Total}元')
